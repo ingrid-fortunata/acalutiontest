752ffa16b12aaedf5d5a88e4ca21a8e470dd94c1
ingrid-fortunata <ingrid.fortunata01@gmail.com>
2021-04-15 11:24:34 +0700

    detail pages

--- src/Pages/ProductDetails.js
+++ src/Pages/ProductDetails.js
@@ -1,6 +1,8 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import Buttons from "../Components/Buttons/Buttons";
+import "./ProductDetails.css";
 
 function ProductDetails(props) {
   const location = useLocation();
@@ -18,11 +20,88 @@ function ProductDetails(props) {
       .then((res) => setWineDetails(res.data.value));
   };
 
-  fetchWineDetails();
+  // fetchWineDetails();
+
+  useEffect(() => {
+    fetchWineDetails();
+  }, []);
 
   return (
     <div>
-      <div>{wineDetails.name}</div>
+      <div className="productdetails-container">
+        <div className="breadcrumbs">
+          Home &#9658; Products &#9658; {wineDetails.grapeVarieties} &#9658;{" "}
+          {wineDetails.country}{" "}
+        </div>
+
+        <div className="productdetails-body">
+          <div style={{ display: "inline-block" }}>
+            <img src={wineDetails.image} alt="winedetails" />
+          </div>
+
+          <div className="productdetails-rightside">
+            <div className="productdetails-title">{wineDetails.name}</div>
+
+            <div className="productdetails-text">
+              {wineDetails.grapeVarieties}{" "}
+              {wineDetails.vintageYear === 0
+                ? "Non Vintage"
+                : wineDetails.vintageYear}
+            </div>
+
+            <div className="productdetails-buy">
+              <div className="productdetails-text">
+                S$ {wineDetails.price}{" "}
+                {wineDetails.qty <= 5 && wineDetails.qty > 0 ? (
+                  <span>{wineDetails.qty} left</span>
+                ) : null}
+              </div>
+              <Buttons name={wineDetails.name} qty={wineDetails.qty} />
+            </div>
+
+            <table className="productdetails-table">
+              <tr className="productdetails-table-title">
+                <td>Region</td>
+                <td>Producer</td>
+                <td>Bottle</td>
+                <td>Alcohol</td>
+              </tr>
+              <tr className="productdetails-table-info">
+                <td>
+                  {wineDetails.country} - {wineDetails.region}
+                </td>
+                <td>{wineDetails.producer}</td>
+                <td>{wineDetails.bottleSize}mL</td>
+                <td>{wineDetails.alcohol}%</td>
+              </tr>
+              <tr></tr>
+              <tr></tr>
+              <tr></tr>
+              <tr>
+                <td colSpan="4" className="productdetails-table-title">
+                  Description
+                </td>
+              </tr>
+              <tr>
+                <td colSpan="4" className="productdetails-table-info">
+                  {wineDetails.description}
+                </td>
+              </tr>
+              <tr></tr>
+              <tr>
+                <td colSpan="4" className="productdetails-table-title">
+                  Tasting Notes
+                </td>
+              </tr>
+              <tr>
+                <td colSpan="4" className="productdetails-table-info">
+                  {wineDetails.tastingNotes}
+                </td>
+              </tr>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
